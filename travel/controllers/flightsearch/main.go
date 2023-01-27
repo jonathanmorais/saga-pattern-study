61319455afda576ff5b92775f19f4b93a08f8ce3
jonathanmorais <jonathan.m.lucena@gmail.com>
2023-01-27 16:45:58 -0300

    flight service

---
+++ travel/controllers/flightsearch/main.go
@@ -0,0 +1,34 @@
+package flightsearch
+
+import (
+	"context"
+	"log"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/segmentio/kafka-go"
+)
+
+func SagaProducer(c *gin.Context) {
+	topic := "flighttopic"
+	partition := 0
+
+	conn, err := kafka.DialLeader(context.Background(), "tcp", "kafka:9092", topic, partition)
+	if err != nil {
+		log.Fatal("failed to dial leader:", err)
+	}
+
+	conn.SetWriteDeadline(time.Now().Add(10 * time.Second))
+	_, err = conn.WriteMessages(
+		kafka.Message{Value: []byte("one!")},
+		kafka.Message{Value: []byte("two!")},
+		kafka.Message{Value: []byte("three!")},
+	)
+	if err != nil {
+		log.Fatal("failed to write messages:", err)
+	}
+
+	if err := conn.Close(); err != nil {
+		log.Fatal("failed to close writer:", err)
+	}
+}
--- travel/main.go
+++ travel/main.go
@@ -2,11 +2,14 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/jonathanmorais/saga-pattern-study/travel/controllers/flightsearch"
 	"github.com/jonathanmorais/saga-pattern-study/travel/controllers/healthchecker"
 )
 
 func main() {
 	router := gin.New()
 	router.POST("/health'", healthchecker.Ok)
-	router.POST("/flightsaga", flightsaga.SagaProducer)
+	router.POST("/flightsaga", flightsearch.SagaProducer)
+	router.Run()
+
 }
