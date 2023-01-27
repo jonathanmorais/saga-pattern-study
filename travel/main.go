16d3c1b0af38b7bd5aba8794599c179fc435cd34
jonathanmorais <jonathan.m.lucena@gmail.com>
2023-01-27 16:03:01 -0300

    flight service

--- travel/main.go
+++ travel/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
-	"github.com/jonathanmorais/saga-pattern-study/travel/controllers/flightsaga"
 	"github.com/jonathanmorais/saga-pattern-study/travel/controllers/healthchecker"
 )
 
