2f13fd0b16e62375a342e15a080633553774dd54
jonathanmorais <jonathan.m.lucena@gmail.com>
2023-01-29 19:21:54 -0300

    flight search service

--- flightsaga/main.go
+++ flightsaga/main.go
@@ -1,48 +1,78 @@
-package flightsaga
+package main
 
 import (
 	"context"
-	"fmt"
-	"strconv"
+	"log"
 	"time"
 
+	"github.com/gin-gonic/gin"
 	"github.com/segmentio/kafka-go"
 )
 
 const (
-	topic          = "flighttopic"
-	broker1Address = "0.0.0.0:9093"
-	broker2Address = "0.0.0.0:9094"
-	broker3Address = "0.0.0.0:9095"
+	topic     = "flighttopic"
+	partition = 0
+	broker    = "localhost"
 )
 
-func SagaProducer(ctx context.Context) {
-	// initialize a counter
-	i := 0
+func BrokerConn() bool {
 
-	// intialize the writer with the broker addresses, and the topic
-	w := kafka.NewWriter(kafka.WriterConfig{
-		Brokers: []string{broker1Address, broker2Address, broker3Address},
-		Topic:   topic,
-	})
-
-	for {
-		// each kafka message has a key and value. The key is used
-		// to decide which partition (and consequently, which broker)
-		// the message gets published on
-		err := w.WriteMessages(ctx, kafka.Message{
-			Key: []byte(strconv.Itoa(i)),
-			// create an arbitrary message payload for the value
-			Value: []byte("search flight" + strconv.Itoa(i)),
-		})
-		if err != nil {
-			panic("could not write message " + err.Error())
-		}
-
-		// log a confirmation once the message is written
-		fmt.Println("writes:", i)
-		i++
-		// sleep for a second
-		time.Sleep(time.Second)
+	conn, err := kafka.DialLeader(context.Background(), "tcp", broker+":9092", topic, partition)
+	if err != nil {
+		log.Fatal("failed to dial leader:", err)
 	}
+
+	conn.SetWriteDeadline(time.Now().Add(10 * time.Second))
+	_, err = conn.WriteMessages(
+		kafka.Message{Value: []byte("one!")},
+		kafka.Message{Value: []byte("two!")},
+		kafka.Message{Value: []byte("three!")},
+	)
+	if err != nil {
+		log.Fatal("failed to write messages:", err)
+	}
+
+	if err := conn.Close(); err != nil {
+		log.Fatal("failed to close writer:", err)
+	}
+
+	return true
+}
+
+func SagaProducer(c *gin.Context) {
+
+	conn := BrokerConn()
+	if conn == false {
+		log.Fatal("failed to take connection with broker:", conn)
+	}
+
+	// make a writer that produces to topic-A, using the least-bytes distribution
+	w := &kafka.Writer{
+		Addr:     kafka.TCP(broker+":9092", broker+":9093", broker+":9094"),
+		Topic:    topic,
+		Balancer: &kafka.LeastBytes{},
+	}
+
+	err := w.WriteMessages(context.Background(),
+		kafka.Message{
+			Key:   []byte("Flight"),
+			Value: []byte("San Francisco"),
+		},
+	)
+	log.Println(w.Logger)
+	if err != nil {
+		log.Fatal("failed to write messages:", err)
+	}
+
+	if err := w.Close(); err != nil {
+		log.Fatal("failed to close writer:", err)
+	}
+
+}
+
+func main() {
+	router := gin.New()
+	router.POST("/flightservice", SagaProducer)
+	router.Run(":8070")
+
 }
